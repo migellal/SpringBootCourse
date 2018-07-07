e20323f50b6b1535c4bb6f274245ac93d3398fe9
migellal <michalgellert@gmail.com>
2018-07-07 17:00:56 +0200

    available courses controller

--- src/main/java/pl/michalgellert/springbootcourse/CourseController.java
+++ src/main/java/pl/michalgellert/springbootcourse/CourseController.java
@@ -7,14 +7,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/course")
 public class CourseController {
 
+    private List<Course> courses = new ArrayList<>();
+
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public ResponseEntity<Long> createCourse(@RequestBody Course course) {
+    public ResponseEntity<Course> createCourse(@RequestBody Course course) {
+        courses.add(course);
         System.out.println(course.getName());
         System.out.println(course.getLengthInSecond());
-        return new ResponseEntity<>(0L, HttpStatus.CREATED);
+        return new ResponseEntity<>(course, HttpStatus.CREATED);
     }
+
+    @RequestMapping(value = "/available", method = RequestMethod.GET)
+    public ResponseEntity<List<Course>> getAvailableCourses() {
+        return new ResponseEntity<>(courses, HttpStatus.OK);
+    }
+
+//    @RequestMapping(value = "/bought", method = RequestMethod.GET) FIXME
+//    public ResponseEntity<List<Course>> getBoughtCourses() {
+//        return new ResponseEntity<>(courses, HttpStatus.OK);
+//    }
 }
